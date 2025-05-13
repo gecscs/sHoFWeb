210cfe8f8d970aa51c62e1f8f499839a58ea6ea6
GECSCS <gecscs.dev@gmail.com>
2025-05-13 10:40:46 +0100

    Corrected the scheduled parameter logic

--- HoFSimpleJSONReader/Repositories/ScreenshotItemRepository.cs
+++ HoFSimpleJSONReader/Repositories/ScreenshotItemRepository.cs
@@ -27,39 +27,67 @@ namespace HoFSimpleJSONReader.Repositories
         {
             var s = await _context.Screenshots.FirstOrDefaultAsync(c => c.Id == shot.Id);
 
-            if (s != null && !scheduled)
+            if (!scheduled)
             {
-                s.Favorited = shot.Favorited;
-                s.FavoritesCount = shot.FavoritesCount;
-                s.FavoritesPerDay = shot.FavoritesPerDay;
-                s.FavoritingPercentage = shot.FavoritingPercentage;
-                s.ViewsCount = shot.ViewsCount;
-                s.ViewsPerDay = shot.ViewsPerDay;
-                s.IsReported = shot.IsReported;
-                s.IsApproved = shot.IsApproved;
+                if (s != null)
+                {
+                    s.Favorited = shot.Favorited;
+                    s.FavoritesCount = shot.FavoritesCount;
+                    s.FavoritesPerDay = shot.FavoritesPerDay;
+                    s.FavoritingPercentage = shot.FavoritingPercentage;
+                    s.ViewsCount = shot.ViewsCount;
+                    s.ViewsPerDay = shot.ViewsPerDay;
+                    s.IsReported = shot.IsReported;
+                    s.IsApproved = shot.IsApproved;
+                }
+                else
+                {
+                    // Handle potential duplicate Creator
+                    if (shot.Creator != null)
+                    {
+                        var existingCreator = await _context.Creators.FindAsync(shot.Creator.Id);
+                        if (existingCreator != null)
+                        {
+                            // Attach existing Creator (reuse tracked one)
+                            shot.Creator = existingCreator;
+                        }
+                        else
+                        {
+                            // Insert new Creator
+                            _context.Creators.Add(shot.Creator);
+                        }
+                    }
+
+                    _context.Screenshots.Add(shot);
+                }
+
+                await _context.SaveChangesAsync();
             }
             else
             {
-                // Handle potential duplicate Creator
-                if (shot.Creator != null)
+                if (s == null)
                 {
-                    var existingCreator = await _context.Creators.FindAsync(shot.Creator.Id);
-                    if (existingCreator != null)
+                    // Handle potential duplicate Creator
+                    if (shot.Creator != null)
                     {
-                        // Attach existing Creator (reuse tracked one)
-                        shot.Creator = existingCreator;
+                        var existingCreator = await _context.Creators.FindAsync(shot.Creator.Id);
+                        if (existingCreator != null)
+                        {
+                            // Attach existing Creator (reuse tracked one)
+                            shot.Creator = existingCreator;
+                        }
+                        else
+                        {
+                            // Insert new Creator
+                            _context.Creators.Add(shot.Creator);
+                        }
                     }
-                    else
-                    {
-                        // Insert new Creator
-                        _context.Creators.Add(shot.Creator);
-                    }
-                }
 
-                _context.Screenshots.Add(shot);
+                    _context.Screenshots.Add(shot);
+                }
             }
 
-            await _context.SaveChangesAsync();
+            
         }
 
         public async Task AddScreenshotDataPointAsync(ScreenshotDataPoint dataPoint)
